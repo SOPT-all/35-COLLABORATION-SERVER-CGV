548d63cd5abfd595ba29420ab2704b4bf8234c88
Yerin Lee <91695537+lreowy@users.noreply.github.com>
2024-11-15 16:53:06 +0900

    initial commit

---
+++ cdsp/src/main/java/com/and_sopt/cdsp/CdspApplication.java
@@ -0,0 +1,13 @@
+package com.and_sopt.cdsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdspApplication.class, args);
+	}
+
+}
---
+++ cdsp/src/test/java/com/and_sopt/cdsp/CdspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.and_sopt.cdsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
