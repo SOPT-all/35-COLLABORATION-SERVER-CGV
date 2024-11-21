38b1c86e117f5cd8b3d13619464a993fb663176f
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-11-21 19:21:24 +0900

    fix : 엔티티 수정

--- src/main/java/com/and_sopt/cdsp/api/movie/domain/Movie.java
+++ src/main/java/com/and_sopt/cdsp/api/movie/domain/Movie.java
@@ -1,6 +1,15 @@
 package com.and_sopt.cdsp.api.movie.domain;
 
 import com.and_sopt.cdsp.api.theater.domain.Theater;
+import com.and_sopt.cdsp.global.common.BaseTimeEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
@@ -14,6 +23,7 @@ public class Movie {
 
     @ManyToOne
     @JoinColumn(name = "theater_id", nullable = false)
+    @JsonIgnore
     private Theater theater;
 
     private LocalDateTime startTime;
