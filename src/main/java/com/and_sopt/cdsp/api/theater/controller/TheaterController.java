9c6ff014874eadfbcc5f7a1c9456f17d37293504
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-11-21 19:22:05 +0900

    feat : 영화 시간표 조회 기능 구현

--- src/main/java/com/and_sopt/cdsp/api/theater/controller/TheaterController.java
+++ src/main/java/com/and_sopt/cdsp/api/theater/controller/TheaterController.java
@@ -1,6 +1,12 @@
 package com.and_sopt.cdsp.api.theater.controller;
 
+import com.and_sopt.cdsp.api.theater.dto.response.MovieTimelineGetResponse;
 import com.and_sopt.cdsp.api.theater.service.TheaterService;
+import com.and_sopt.cdsp.global.response.ApiResponseDto;
+import com.and_sopt.cdsp.global.response.enums.SuccessCode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +17,12 @@ public class TheaterController {
     public TheaterController(TheaterService theaterService) {
         this.theaterService = theaterService;
     }
+
+    @GetMapping("/theater/{theaterId}/movie")
+    public ApiResponseDto<MovieTimelineGetResponse> getMovieTimeline(@PathVariable("theaterId") final Long theaterId,
+                                                                     @RequestParam("auditorium") final String auditorium,
+                                                                     @RequestParam("auditoriumType") final String auditoriumType) {
+        return ApiResponseDto.success(SuccessCode.MOVIE_TIMELINE_GET_SUCCESS, theaterService.getMovieTimeline(theaterId, auditorium, auditoriumType));
+    }
+
 }
--- src/main/java/com/and_sopt/cdsp/api/theater/service/TheaterService.java
+++ src/main/java/com/and_sopt/cdsp/api/theater/service/TheaterService.java
@@ -1,7 +1,34 @@
 package com.and_sopt.cdsp.api.theater.service;
 
+import com.and_sopt.cdsp.api.movie.domain.Movie;
+import com.and_sopt.cdsp.api.movie.repository.MovieRepository;
+import com.and_sopt.cdsp.api.movie.service.MovieService;
+import com.and_sopt.cdsp.api.theater.dto.response.MovieTimelineGetResponse;
+import com.and_sopt.cdsp.api.theater.domain.Theater;
+import com.and_sopt.cdsp.api.theater.repository.TheaterRepository;
+import com.and_sopt.cdsp.global.exception.CustomException;
+import com.and_sopt.cdsp.global.response.enums.ErrorCode;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TheaterService {
+
+    private final TheaterRepository theaterRepository;
+    private final MovieRepository movieRepository;
+
+    public TheaterService(TheaterRepository theaterRepository, MovieRepository movieRepository) {
+        this.theaterRepository = theaterRepository;
+        this.movieRepository = movieRepository;
+    }
+
+    public MovieTimelineGetResponse getMovieTimeline(Long theaterId, String auditorium, String auditoriumType) {
+        Theater theater = theaterRepository.findById(theaterId)
+                .orElseThrow(
+                        () ->  new CustomException(ErrorCode.THEATER_NOT_FOUND));
+        List<Movie> movieList = movieRepository.findByTheaterIdAndAuditoriumAndAuditoriumType(theaterId, auditorium, auditoriumType);
+        return MovieTimelineGetResponse.builder()
+                .movieList(movieList)
+                .build();
+    }
 }
