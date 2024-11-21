9ebc364ed1ec24ed870da3a19f17ff0586ac2c12
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-11-21 19:21:03 +0900

    feat : 에러코드 추가
    # Conflicts:
    #	src/main/java/com/and_sopt/cdsp/global/response/enums/ErrorCode.java

--- src/main/java/com/and_sopt/cdsp/global/response/enums/ErrorCode.java
+++ src/main/java/com/and_sopt/cdsp/global/response/enums/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
     MISSING_REQUIRED_PARAMETER(40002, HttpStatus.BAD_REQUEST, "필수 파라미터가 누락되었습니다."),
     //404 Not Found
     NOT_FOUND(40400, HttpStatus.NOT_FOUND, "존재하지 않는 API입니다."),
+    THEATER_NOT_FOUND(40402, HttpStatus.NOT_FOUND, "존재하지 않는 영화관입니다."),
     NOT_FOUND_MOVIE(40401, HttpStatus.NOT_FOUND, "존재하지 않는 영화 ID 입니다."),
     //405 Method Not Allowed
     METHOD_NOT_ALLOWED(40500, HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 메소드입니다."),
