cc352927d9f83e0a32e035d563aa581149e6bf2a
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-11-21 19:29:13 +0900

    chore : 리베이스 과정에서 변경 사항 누락된거 추가

--- src/main/java/com/and_sopt/cdsp/api/movie/repository/MovieRepository.java
+++ src/main/java/com/and_sopt/cdsp/api/movie/repository/MovieRepository.java
@@ -5,5 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findMoviesByTheaterId(Long theaterId);
     List<Movie> findByTheaterIdAndAuditoriumAndAuditoriumType(Long theaterId, String auditorium, String auditoriumType);
 }
