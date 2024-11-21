b57c7c1ade8047f423fe2b47b7577353045fd244
Yeonnies <eliyeon16@gmail.com>
2024-11-21 20:51:52 +0900

    [FEAT] 영화관 조회 api 구현

--- src/main/java/com/and_sopt/cdsp/api/theater/service/TheaterService.java
+++ src/main/java/com/and_sopt/cdsp/api/theater/service/TheaterService.java
@@ -1,7 +1,18 @@
 package com.and_sopt.cdsp.api.theater.service;
 
+import com.and_sopt.cdsp.api.theater.domain.Theater;
+import com.and_sopt.cdsp.api.theater.repository.TheaterRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TheaterService {
+    private final TheaterRepository theaterRepository;
+    public TheaterService(TheaterRepository theaterRepository) {
+        this.theaterRepository = theaterRepository;
+    }
+    public List<Theater> getAllTheaters() {
+        return theaterRepository.findAll();
+    }
 }
