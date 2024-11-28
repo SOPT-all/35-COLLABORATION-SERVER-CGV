ff097917f03e6e3c3f131bf1526cb5cd2dfd368d
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-11-28 15:36:44 +0900

    hotfix : import 오류 수정

--- src/main/java/com/and_sopt/cdsp/api/movie/domain/Movie.java
+++ src/main/java/com/and_sopt/cdsp/api/movie/domain/Movie.java
@@ -4,6 +4,7 @@ import com.and_sopt.cdsp.api.theater.domain.Theater;
 
 import com.and_sopt.cdsp.api.ticket.domain.Ticket;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,6 +12,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.List;
 
